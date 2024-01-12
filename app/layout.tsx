d42cde9fd6181bb4405d6d4dd8011e3386b21a91
CinemaCafe <65265249+CinemaCafe@users.noreply.github.com>
2024-01-13 01:14:19 +0800

    (feat) : update to nextjs 14 1. update nextjs framework version. 2. add viewport. 3. add api agent.

--- app/layout.tsx
+++ app/layout.tsx
@@ -14,16 +14,19 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s - ${siteConfig.name}`,
   },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
   icons: {
     icon: "/favicon.ico",
     shortcut: "/favicon-16x16.png",
     apple: "/apple-touch-icon.png",
   },
+}
+
+// for nextjs 14
+export const viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 }
 
 interface RootLayoutProps {
